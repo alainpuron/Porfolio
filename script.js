b9c80585cfd7f3762be3b86b5413fa847a00d53a
alainpuron <102711860+alainpuron@users.noreply.github.com>
2023-06-04 10:20:54 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// if the form is empy the thank you alert does not show
+function mailSent() {
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  
+  if (email !== "" && message !== "") {
+    alert("Your Email was sent to Alain Puron. Thank you!");
+  }
+}
+
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+});
